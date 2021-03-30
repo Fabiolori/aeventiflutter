eb62f72fc00d25393cbfafb9644427a488bd7545
Fabiolori <73469335+Fabiolori@users.noreply.github.com>
2021-03-30 22:38:21 +0200

    Initial commit: -Progetto di avvio dell'applicazione

---
+++ android/app/src/main/kotlin/com/example/aeventi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aeventi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/unicam/aeventi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.unicam.aeventi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
